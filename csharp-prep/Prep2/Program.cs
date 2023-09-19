f1c14b59a916be09a0d4cfe4764a26a0eb1d9c07
lidya langana <lidyalangana1@gmail.com>
2023-09-19 10:13:16 -0600

    edit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("what's your grade?");
+        string answer = Console.ReadLine();
+        int percent =int.Parse(answer);
+
+        string letter ="";
+
+        if( percent >= 90)
+        {
+            letter ="A";
+        }
+
+        else if (percent >=80)
+        {
+            letter = "B";
+        }
+
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if(percent >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+       Console.WriteLine($"your grade is :{letter}");
+
+       if(percent >= 70)
+       {
+        Console.Write(" congrats you have passed");
+       }
+       else
+       {
+        Console.WriteLine("Better luck next time");
+       }
+
+
     }
 }
